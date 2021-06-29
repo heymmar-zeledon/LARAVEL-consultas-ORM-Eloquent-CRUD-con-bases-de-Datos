4df49fc8e9064ccf95574db6500350a079b9cf1d
heymmar-zeledon <Eliazithzeledon27@gmail.com>
2021-06-29 12:48:15 -0600

    Añadiendo front-end principal al proyecto con livewire y plantillas blade

---
+++ resources/views/livewire/plantilla-main.blade.php
@@ -0,0 +1,12 @@
+<div class="container pt-2">
+    <div class="a1">
+        <H4 class="border border-success" id="h4"> BASES DE DATOS RELACIONALES</H4>
+        <a type="button" class="btn btn-primary" style="width:40%;" href="{{url('/')}}">Home</a>
+    </div>
+</div>
+<br>
+<div class="container" style="padding-bottom: 40px;">
+        <div class=" col-sm-12 alert alert-secondary" role="alert">
+            @yield('contenido')
+        </div>
+</div>
